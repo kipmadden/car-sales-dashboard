dc4c0bc95ed432b6f0f7ec33ebb7c8f7a0e9511d
kipmadden <kipmadden@cosapient.com>
2025-05-18 14:15:54 -0500

    replaced tabs with tab_list, panels with tab_panels

--- car_sales_dashboard/pages/index.py
+++ car_sales_dashboard/pages/index.py
@@ -43,23 +43,20 @@ def index():
                 
                 # Tabs for different views
                 rx.tabs(
-                    tabs=[
-                        rx.tab("Sales Forecast", id="sales", on_click=lambda: DashboardState.set_active_tab("sales")),
-                        rx.tab("Vehicle Analysis", id="vehicles", on_click=lambda: DashboardState.set_active_tab("vehicles")),
-                        rx.tab("Geographic", id="geographic", on_click=lambda: DashboardState.set_active_tab("geographic")),
-                        rx.tab("Economic Factors", id="economic", on_click=lambda: DashboardState.set_active_tab("economic")),
-                    ],
-                    panels=[
+                    rx.tab_list(
+                        rx.tab("Sales Forecast", id="sales"),
+                        rx.tab("Vehicle Analysis", id="vehicles"),
+                        rx.tab("Geographic", id="geographic"),
+                        rx.tab("Economic Factors", id="economic"),
+                    ),
+                    rx.tab_panels(
                         rx.tab_panel(
                             rx.vstack(
-                                # Main sales trend and forecast chart
                                 chart_container(
                                     "Sales Trend and Forecast",
                                     get_sales_trend_chart(),
                                     height="500px"
                                 ),
-                                
-                                # Toggle for forecast table
                                 rx.hstack(
                                     rx.switch(
                                         on_change=DashboardState.toggle_table,
@@ -68,82 +65,62 @@ def index():
                                     rx.text("Show Forecast Table"),
                                     margin_top="1em",
                                 ),
-                                
-                                # Conditional forecast table
                                 rx.cond(
                                     DashboardState.show_table,
                                     create_forecast_table(pd.DataFrame(DashboardState.forecast_data)),
                                     rx.text("")
                                 ),
-                                
                                 width="100%",
                             ),
                             id="sales-panel",
                         ),
-                        
                         rx.tab_panel(
                             rx.vstack(
                                 rx.hstack(
-                                    # Vehicle type chart
                                     chart_container(
                                         "Sales by Vehicle Type",
                                         get_vehicle_type_chart(),
                                         height="400px"
                                     ),
-                                    
-                                    # Top models chart
                                     chart_container(
                                         "Top Models by Sales",
                                         get_top_models_chart(),
                                         height="400px"
                                     ),
-                                    
                                     width="100%",
                                 ),
-                                
-                                # Sales heatmap
                                 chart_container(
                                     "Sales by Month and Vehicle Type",
                                     get_sales_by_month_chart(),
                                     height="400px"
                                 ),
-                                
                                 width="100%",
                             ),
                             id="vehicles-panel",
                         ),
-                        
                         rx.tab_panel(
                             rx.vstack(
-                                # Region chart
                                 chart_container(
                                     "Sales by Region",
                                     get_region_chart(),
                                     height="400px"
                                 ),
-                                
-                                # State map
                                 chart_container(
                                     "Sales by State",
                                     get_state_map_chart(),
                                     height="500px"
                                 ),
-                                
                                 width="100%",
                             ),
                             id="geographic-panel",
                         ),
-                        
                         rx.tab_panel(
                             rx.vstack(
-                                # Exogenous variable impact
                                 chart_container(
                                     "Exogenous Variable Trends",
                                     get_exogenous_impact_chart(),
                                     height="500px"
                                 ),
-                                
-                                # Summary table
                                 rx.box(
                                     create_summary_table(
                                         pd.DataFrame(DashboardState.filtered_data),
@@ -156,12 +133,11 @@ def index():
                                     border="1px solid #EEE",
                                     margin_top="1em",
                                 ),
-                                
                                 width="100%",
                             ),
                             id="economic-panel",
                         ),
-                    ],
+                    ),
                     width="100%",
                     variant="enclosed",
                     margin_top="1em",
