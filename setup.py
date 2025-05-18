4daa54df2a1c1756fcaa8b604e84e680ba2255b3
Kip Madden <56366836+kipmadden@users.noreply.github.com>
2025-05-18 01:49:28 -0500

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,11 @@ setup(
     name="car_sales_dashboard",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        "reflex",
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "plotly",
+    ],
 )
