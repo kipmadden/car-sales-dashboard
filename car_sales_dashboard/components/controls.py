692609aac624e1e50f14172c1b3d9ddcd1257f84
kipmadden <kipmadden@cosapient.com>
2025-05-18 20:15:40 -0500

    changed all usage of 'sm', 'md', 'lg' to numeric values between 1-9

--- car_sales_dashboard/components/controls.py
+++ car_sales_dashboard/components/controls.py
@@ -201,25 +201,26 @@ def chart_container(title, chart_data, height="400px"):
         chart_data: Var containing a plot dictionary
         height (str): Chart height
     
-    Returns:
-        rx.Component: Chart container component
-    """    # We use rx.cond to handle the case when chart_data might be empty
-    return rx.box(
-        rx.heading(title, color="black", size="md"),
-        # The key change: use rx.cond to handle empty data case
-        rx.cond(
-            chart_data == {},
-            rx.center("No data available for this selection", height="200px", color="black"),
-            rx.plotly(
-                figure=chart_data,
-                height=height
-            )
-        ),
-        width="100%",
-        padding="1em",
-        background="white",
-        border_radius="md",
-        border="1px solid #EEE",
-        margin_top="1em",
-        height=height,
-    )
+Returns:
+    rx.Component: Chart container component
+"""
+# We use rx.cond to handle the case when chart_data might be empty
+return rx.box(
+    rx.heading(title, color="black", size="4"),
+    # The key change: use rx.cond to handle empty data case
+    rx.cond(
+        chart_data == {},
+        rx.center("No data available for this selection", height="200px", color="black"),
+        rx.plotly(
+            figure=chart_data,
+            height=height
+        )
+    ),
+    width="100%",
+    padding="1em",
+    background="white",
+    border_radius="md",
+    border="1px solid #EEE",
+    margin_top="1em",
+    height=height,
+)
